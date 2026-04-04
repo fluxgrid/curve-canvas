5c0afe0450c206c1076ae139ff7890430007d731
Lawrence Lee <llee123@gmail.com>
2026-04-04 13:00:53 -0700

    Add mouse pan controls to playtest freecam

--- addons/curve_canvas/Runtime/FreecamController.cs
+++ addons/curve_canvas/Runtime/FreecamController.cs
@@ -7,6 +7,9 @@ public partial class FreecamController : Camera3D
     [Export] public float MoveSpeed { get; set; } = 15f;
     [Export] public float BoostMultiplier { get; set; } = 3f;
     [Export] public float ScrollSpeed { get; set; } = 20f;
+    [Export] public float MousePanSpeed { get; set; } = 0.002f;
+
+    private bool _isMousePanning;
 
     public override void _Ready()
     {
@@ -23,19 +26,36 @@ public partial class FreecamController : Camera3D
 
     public override void _UnhandledInput(InputEvent @event)
     {
-        if (@event is not InputEventMouseButton mouse || !mouse.Pressed)
+        if (@event is InputEventMouseButton mouse)
         {
-            return;
+            if (mouse.ButtonIndex == MouseButton.WheelUp && mouse.Pressed)
+            {
+                Translate(new Vector3(0f, 0f, -ScrollSpeed));
+                GetViewport()?.SetInputAsHandled();
+                return;
+            }
+
+            if (mouse.ButtonIndex == MouseButton.WheelDown && mouse.Pressed)
+            {
+                Translate(new Vector3(0f, 0f, ScrollSpeed));
+                GetViewport()?.SetInputAsHandled();
+                return;
+            }
+
+            if (mouse.ButtonIndex == MouseButton.Middle)
+            {
+                _isMousePanning = mouse.Pressed;
+                GetViewport()?.SetInputAsHandled();
+                return;
+            }
         }
 
-        if (mouse.ButtonIndex == MouseButton.WheelUp)
+        if (@event is InputEventMouseMotion motion && _isMousePanning)
         {
-            Translate(new Vector3(0f, 0f, -ScrollSpeed));
-            GetViewport()?.SetInputAsHandled();
-        }
-        else if (mouse.ButtonIndex == MouseButton.WheelDown)
-        {
-            Translate(new Vector3(0f, 0f, ScrollSpeed));
+            var depthScale = Mathf.Max(1f, Mathf.Abs(Position.Z));
+            var delta = motion.Relative;
+            var movement = new Vector3(-delta.X, delta.Y, 0f) * MousePanSpeed * depthScale;
+            Translate(movement);
             GetViewport()?.SetInputAsHandled();
         }
     }
